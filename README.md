c6745e3269e4f8730fc7be9deaa663d30085ae81
Sejal Dua <sejal.dua@tufts.edu>
2020-12-05 20:24:44 -0800

    completed day 5

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 growth season
 
 ### GOALS:
-* elegant code
+* functional programming
 * list comprehensions
-* zero / minimal import libraries
 * code refactoring
 * modularity
-* code style
 * try new languages
 
 ### STATUS
@@ -17,8 +15,8 @@ growth season
 | 1 | [Report Repair](https://adventofcode.com/2020/day/1) | [01](day1/report-repair.py) | ★★ |
 | 2 | [Password Philosophy](https://adventofcode.com/2020/day/2) | [02](day2/password-philosophy.py) | ★★ |
 | 3 | [Toboggan Trajectory](https://adventofcode.com/2020/day/3) | [03](day3/toboggan-trajectory.py) | ★★ |
-| 4 | [Password Processing](https://adventofcode.com/2020/day/4) | [04](day4/password-processing.py) | ★★ | ☆☆ |
-| 5 |  |  | ☆☆ |
+| 4 | [Password Processing](https://adventofcode.com/2020/day/4) | [04](day4/password-processing.py) | ★★ |
+| 5 | [Binary Boarding](https://adventofcode.com/2020/day/5) | [05](day5/binary-boarding.py) | ★★ |
 | 6 |  |  | ☆☆ |
 | 7 |  |  | ☆☆ |
 | 8 |  |  | ☆☆ |
--- day5/binary-boarding.py
+++ day5/binary-boarding.py
@@ -1,69 +1,30 @@
-import re
 
-"""
-REQUIRED FIELDS:
-byr (Birth Year)
-iyr (Issue Year)
-eyr (Expiration Year)
-hgt (Height)
-hcl (Hair Color)
-ecl (Eye Color)
-pid (Passport ID)
-cid (Country ID)
-"""
+def binary_search(s, lo, hi):
+    for char in s:
+        if char == "F" or char == "L":
+            hi -= (hi - lo) // 2 + 1
+        else:
+            lo += (hi - lo) // 2 + 1
+    return lo if char == "F" or char == "L" else hi
+
 def puzzle1():
-    res = 0
+    seat_ids = []
     with open("input.txt", 'r') as file:
-        l = file.read().split('\n\n')
+        l = file.read().split('\n')[:-1]
         for s in l:
-            if s != "":
-                text = s.split()
-                passport = dict()
-                for field in text:
-                    k, v = field.split(":")
-                    passport[k] = v
-                if all([field in passport.keys() for field in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']]):
-                    res += 1
-    return res
+            row = binary_search(s[:7], 0, 127)
+            col = binary_search(s[7:], 0, 7)
+            seat_id = row * 8 + col
+            seat_ids.append(seat_id)
+    return seat_ids
 
-
-"""
-CRITERIA:
-byr (Birth Year) - four digits; at least 1920 and at most 2002.
-iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-hgt (Height) - a number followed by either cm or in:
-If cm, the number must be at least 150 and at most 193.
-If in, the number must be at least 59 and at most 76.
-hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-pid (Passport ID) - a nine-digit number, including leading zeroes.
-"""
 def puzzle2():
-    rules = {
-        'byr': (lambda x: True if int(x) >= 1920 and int(x) <= 2002 else False),
-        'iyr': (lambda x: True if int(x) >= 2010 and int(x) <= 2020 else False), 
-        'eyr': (lambda x: True if int(x) >= 2020 and int(x) <= 2030 else False), 
-        'hgt': (lambda x: True if ("cm" in x and int(x[:x.find("cm")]) >= 150 and int(x[:x.find("cm")]) <= 193) or ("in" in x and int(x[:x.find("in")]) >= 59 and int(x[:x.find("in")]) <= 76) else False),
-        'hcl': (lambda x: True if len(x) == 7 and re.match("^#([a-f0-9]{6})", x) else False),  
-        'ecl': (lambda x: True if x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] else False), 
-        'pid': (lambda x: True if len(x) == 9 and int(x) else False)
-    }
-    res = 0
-    with open("input.txt", 'r') as file:
-        l = file.read().split('\n\n')
-        for s in l:
-            if s != "":
-                text = s.split()
-                passport = dict()
-                for field in text:
-                    k, v = field.split(":")
-                    passport[k] = v
-                checks = {key: rules[key](value) for key, value in passport.items() if key in ['byr', 'iyr', 'eyr', 'hcl', 'hgt', 'ecl', 'pid']}
-                if all([field in passport.keys() for field in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']]) and all(checks.values()):
-                    res += 1
-    return res
+    seat_ids = puzzle1()
+    missing = [i for i in range(max(seat_ids)) if i not in seat_ids]
+    for x in missing:
+        if (x - 1) not in missing and (x + 1) not in missing:
+            return x
 
 if __name__ == "__main__":
-    print(puzzle1())
+    print(max(puzzle1()))
     print(puzzle2())
